fbdcf5c2465c9e0cec83c523fed8c2b9bd096fdb
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2023-07-26 19:10:19 +0530

    Linked List - 2

--- LinkedList.py
+++ LinkedList.py
@@ -11,6 +11,17 @@ class LinkedList:
         newNode = Node(data,self.head)
         self.head = newNode
         
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+            return
+        hptr = self.head
+        while hptr:
+            if hptr.next is None:
+                hptr.next = Node(data,None)
+                break
+            hptr = hptr.next
+        
     def Print_List(self):
         hptr = self.head
         Lstr = ""
@@ -25,9 +36,11 @@ class LinkedList:
         Lstr += 'None'
         print(Lstr)
         
+        
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert_at_start(10)
-    ll.insert_at_start(20)
-    ll.insert_at_start(30)
+    ll.insert_at_end(10)
+    ll.insert_at_end(20)
+    ll.insert_at_end(30)
+    ll.insert_at_end(40)
     ll.Print_List()
